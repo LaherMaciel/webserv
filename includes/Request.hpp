95c1e62b43b7019ff90f48b6b6efae2cc46ac4e2
belinore <belinore@student.42lisboa.com>
2026-09-17 19:02:03 +0100

    exception handling updated

--- includes/Request.hpp
+++ includes/Request.hpp
@@ -28,6 +28,7 @@ class Request
         std::string method_;
         std::string path_;
         std::string version_;
+        std::string body_;
         std::map<std::string, std::string> headers_;
 };
 
